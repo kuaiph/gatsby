4ae009e0930b8e075f077c90933acae3051bdc2b
Jarmo Isotalo <jamo@isotalo.fi>
2019-05-31 13:07:40 +0300

    FIX

--- packages/gatsby-telemetry/src/error-helpers.js
+++ packages/gatsby-telemetry/src/error-helpers.js
@@ -4,8 +4,11 @@ const { sep } = require(`path`)
 const cleanPaths = (str, separator = sep) => {
   const stack = process.cwd().split(separator)
 
-  // windows still has the 'C:' but unix has '' but C:
-  // is not a big deal so we'll let it slip
+  // Since the error is JSON.stringified the `\` is escaped so we need to take that in to account
+  if (separator === `\\`) {
+    separator = `\\\\`
+  }
+
   while (stack.length > 1) {
     const currentPath = stack.join(separator)
     const currentRegex = new RegExp(
